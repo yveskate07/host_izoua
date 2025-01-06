55fe8f817690e9d4873cbc08022f8781dc2e3066
yveskate07 <118679761+yveskate07@users.noreply.github.com>
2025-01-06 11:16:14 +0000

    Update settings.py

--- izouaproject/settings.py
+++ izouaproject/settings.py
@@ -40,9 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'livereload',
     'izouapp',
-    'phonenumber_field',
     "accounts",
 ]
 
